382124fac0af3624ec8ca37dfa6057de2301dc84
Michael Dooley <94997943+mido1387@users.noreply.github.com>
2025-10-07 14:11:53 +0200

    typo

--- occult_part1_orca.s
+++ occult_part1_orca.s
@@ -1,6 +1,6 @@
 #!/bin/bash
 # PART 1: Initial ORCA Jobs (MPI Parallel)
-#SBATCH --job-name=v4_test_occult_part1
+#SBATCH --job-name=occult_part1
 #SBATCH --output=occult_part1.out
 #SBATCH --error=occult_part1.err
 #SBATCH --nodes=1
